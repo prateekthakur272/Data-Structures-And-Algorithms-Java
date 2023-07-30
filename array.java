5cc17d82ba29ed43bee5c17e07d6f8337086d7cc
Prateek Thakur <prateekthakur272@gmail.com>
2023-07-31 00:04:00 +0530

    array

---
+++ array.java
@@ -0,0 +1,10 @@
+class Array{
+    public static void main(String args[]){
+        int arr[] = {1,2,4,2,5,7,4,6,6,8,3,1};
+        for (int a : arr) {
+            System.out.printf("%d%c",a,'\t');
+        }
+        System.out.println();
+        System.out.println(arr[3]);
+    }
+}
